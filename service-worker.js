74060f4f79a55107f95c1ec4afe064a8dbdff9db
mensfitnessplus <mensfitnessplus@gmail.com>
2026-06-17 18:49:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ca-tracker-v1.3";
+const CACHE_NAME = "ca-tracker-v1.4";
 
 const urlsToCache = [
   "./",
@@ -7,16 +7,54 @@ const urlsToCache = [
   "./icon.png"
 ];
 
+// Force immediate installation of the new service worker
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+// Clean up old caches completely so old files are deleted
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
+});
+
+// Use a Network-First strategy for core files so updates apply instantly when online
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  const isCoreAsset = urlsToCache.some(path => {
+    const cleanPath = path.replace("./", "");
+    return cleanPath === "" ? url.pathname === "/" : url.pathname.endsWith(cleanPath);
+  });
+
+  if (isCoreAsset) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          if (response.status === 200) {
+            const resClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+          }
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
